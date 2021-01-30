4c3b57b019822c9bd6000bc760511276c9b25351
Sergi Gisbert <sgisbert@users.noreply.github.com>
2021-01-30 13:07:12 +0100

    long running task

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Threading;
+using System.Diagnostics;
+using System;
 
 namespace parallel
 {
@@ -6,7 +8,23 @@ namespace parallel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Process(i);
+            }
+
+            Console.WriteLine($"Completed: {timer.Elapsed}");
+        }
+
+        private static void Process(int id)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            Thread.Sleep(200);
+            Console.WriteLine($"Process {id}: {timer.Elapsed}");
         }
     }
 }
