c446abc728e858661d99be690e3c43833f11b6d3
Sergi Gisbert <sgisbert@users.noreply.github.com>
2021-01-30 20:40:05 +0100

    Add a returning value to the Process method

--- src/Program.cs
+++ src/Program.cs
@@ -13,25 +13,39 @@ namespace parallel
             Stopwatch timer = new Stopwatch();
             timer.Start();
 
-            List<Task> tasks = new List<Task>();
+            List<Task<int>> tasks = new List<Task<int>>();
             for (int i = 0; i < 10; i++)
             {
                 tasks.Add(Process(i));
             }
             Task.WaitAll(tasks.ToArray());
 
+            List<int> results = new List<int>();
+            foreach (var task in tasks)
+            {
+                results.Add(task.Result);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Result: {string.Join(",", results)}");
             Console.WriteLine($"Completed: {timer.Elapsed}");
         }
 
-        private static async Task Process(int id)
+        private static async Task<int> Process(int id)
         {
-            await Task.Run(() =>
+            var number = await Task<int>.Run(() =>
             {
                 Stopwatch timer = new Stopwatch();
                 timer.Start();
+                Random random = new Random();
                 Thread.Sleep(200);
+
+                int number = random.Next(1,100);
+
                 Console.WriteLine($"Process {id}: {timer.Elapsed}");
+                return number;
             });
+            return number;
         }
     }
 }
